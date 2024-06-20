d0a2d055e9fc1b5d900fd56527a1dde3811f19d5
Rubenvill0811 <rubenvill0811@gmail.com>
2024-06-20 16:48:46 -0700

    finished writing the methods in the DB class and then wrote some api routes.

---
+++ Develop/db/database.js
@@ -0,0 +1,64 @@
+const { error } = require('console');
+const fs = require('fs');
+const util = require('util');
+const uuidv1 = require('uuid/v1');
+
+const createFilePromise = util.promisify(fs.writeFile);
+const readFilePromise = util.promisify(fs.readFile);
+
+class DB {
+    post(note) {
+        return createFilePromise('db/db.json', JSON.stringify(note));
+    }
+    
+    get() {
+        return readFilePromise('db/db.json', 'utf8');
+    }
+
+    async storedNotes() {
+        try {
+            const notes = await this.get();
+            return JSON.parse(notes) || [];
+        } catch (err) {
+            return [];
+        }
+    }
+
+    async createNote(note) {
+        const { title, content } = note;
+        if (!title || !content) {
+            throw new Error('The title or text fields cannot be empty.');
+        }
+
+        const newNote = { title, content, id: uuidv1() };
+        try {
+            const notes = await this.storedNotes();
+            const noteBook = [...notes, newNote];
+            await this.post(noteBook); 
+            return newNote;
+        } catch (err) {
+            throw new Error('There was an error posting this note.');
+        }
+    }
+
+    async deleteNote(id) {
+        try {
+            const notes = await this.storedNotes();
+            const filteredNotes = notes.filter((note) => note.id !== id);
+            await this.write(filteredNotes);
+        } catch (err) {
+            console.error(`Error deleting note: ${err}`)
+                throw error;
+        }
+    }
+
+
+
+
+
+
+
+
+}
+
+module.exports = DB;
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,6 +1,33 @@
 const router = require('express').Router();
+const DB = require('../db/database')
 
 
+router.post('/notes', async (req, res) => {
+    try {
+        const postNote = await DB.createNote(req.body)
+        return res.json(postNote);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
+router.delete('/notes/:id', async (req, res) => {
+    try {
+        const deletedNote = await DB.deleteNote(req.params.id);
+        return res.json(deletedNote);
+    } catch(err) {
+        return res.status(500).json(err)
+    }
+});
+
+router.get('/notes', async (req, res)=>{
+    try {
+        const notes = await DB.storedNotes();
+        return res.json(notes);
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+});
 
 
 module.exports = router;
